7ceab8c50ccd42442a978ee7b8c5e9c421a744df
Niketh23 <nikethmdcl2003@gmail.com>
2024-10-18 11:29:10 +0530

    added zod validation

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.get("/",(req,res)=>{
+app.use(express.json());
+
+app.post("/createTodo",(req,res)=>{
   res.json({
     msg : "Todo list app created"
   })
+})
+
+app.get("/todos",(res,res)=>{
+
+})
+
+app.put("/completed",(req,res)=>{
+  
 })
 
 app.listen(3000);
